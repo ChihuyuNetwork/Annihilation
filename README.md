e21167803e2d6473d4d30eed46e51abc45a5f144
hirosukt <senolynn7584@gmail.com>
2023-04-23 22:32:24 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# KotlinMinecraftPlugin
+Best purpur plugin template for me.  
+※All of settings are tuned for me, probably you unfamiliar write codes at this template.
+
+## Usage
+1. Set minecraft version in `gradle.properties`
+2. Run gradle task `setup`
+3. You are the godly plugin developer
+
+## Format
+You can format codes to very handsome using ktlint.
+1. Configure rules in `build.gradle.kts#ktlint`
+2. Run gradle task `ktlintFormat`
+
+## Test
+You can test plugin in paper with gradle.
+Server data folder generated in `run/`.
+1. Run gradle task `runServer`
---
+++ build.gradle.kts
@@ -0,0 +1,114 @@
+plugins {
+    kotlin("jvm") version "1.8.20"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("org.jlleitschuh.gradle.ktlint") version "11.3.1"
+    id("xyz.jpenilla.run-paper") version "2.0.1"
+    id("io.github.gradle-nexus.publish-plugin") version "1.3.0"
+    `maven-publish`
+    `java-library`
+    `kotlin-dsl`
+    java
+    idea
+}
+
+group = "love.chihuyu"
+version = "0.0.1-SNAPSHOT"
+val pluginVersion: String by project.ext
+
+repositories {
+    mavenCentral()
+    maven("https://repo.codemc.org/repository/maven-public/")
+    maven("https://repo.hirosuke.me/repository/maven-public/")
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+/*
+1.7.10~1.8.8: "org.github.paperspigot:paperspigot-api:$pluginVersion-R0.1-SNAPSHOT"
+1.9.4~1.16.5: "com.destroystokyo.paper:paper-api:$pluginVersion-R0.1-SNAPSHOT"
+1.17~1.19.4: "io.papermc.paper:paper-api:$pluginVersion-R0.1-SNAPSHOT"
+ */
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:$pluginVersion-R0.1-SNAPSHOT")
+    implementation("love.chihuyu:ChihuyuLib:0.1.4")
+    implementation("dev.jorel:commandapi-core:9.0.0")
+    implementation("dev.jorel:commandapi-kotlin:8.8.0")
+    implementation(kotlin("stdlib"))
+}
+
+ktlint {
+    ignoreFailures.set(true)
+    disabledRules.add("no-wildcard-imports")
+}
+
+tasks {
+    test {
+        useJUnitPlatform()
+    }
+
+    processResources {
+        duplicatesStrategy = DuplicatesStrategy.INCLUDE
+
+        from(sourceSets.main.get().resources.srcDirs) {
+            filter(org.apache.tools.ant.filters.ReplaceTokens::class, mapOf("tokens" to mapOf(
+                "version" to project.version.toString(),
+                "name" to project.name,
+                "mainPackage" to "love.chihuyu.${project.name.lowercase()}.${project.name}Plugin"
+            )))
+            filteringCharset = "UTF-8"
+        }
+    }
+
+    shadowJar {
+        val loweredProject = project.name.lowercase()
+        exclude("org/slf4j/**")
+        relocate("kotlin", "love.chihuyu.$loweredProject.lib.kotlin")
+        relocate("dev.jorel.commandapi", "love.chihuyu.$loweredProject.lib.dev.jorel.commandapi")
+    }
+
+    runServer {
+        minecraftVersion(pluginVersion)
+    }
+}
+
+nexusPublishing {
+    this.repositories {
+        create("repo") {
+            nexusUrl.set(uri("https://repo.hirosuke.me/repository/maven-releases/"))
+            snapshotRepositoryUrl.set(uri("https://repo.hirosuke.me/repository/maven-snapshots/"))
+        }
+    }
+}
+
+kotlin {
+    jvmToolchain(18)
+}
+
+open class SetupTask : DefaultTask() {
+
+    @TaskAction
+    fun action() {
+        val projectDir = project.projectDir
+        projectDir.resolve("renovate.json").deleteOnExit()
+        val srcDir = projectDir.resolve("src/main/kotlin/love/chihuyu/${project.name.lowercase()}").apply(File::mkdirs)
+        srcDir.resolve("${project.name}Plugin.kt").writeText(
+            """
+                package love.chihuyu.${project.name.lowercase()}
+                
+                import org.bukkit.plugin.java.JavaPlugin
+
+                class ${project.name}Plugin: JavaPlugin() {
+                    companion object {
+                        lateinit var ${project.name}Plugin: JavaPlugin
+                    }
+                
+                    init {
+                        ${project.name}Plugin = this
+                    }
+                }
+            """.trimIndent()
+        )
+    }
+}
+
+task<SetupTask>("setup")
