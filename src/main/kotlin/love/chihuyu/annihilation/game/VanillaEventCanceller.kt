d5fc54ca44580a06e71cfae615c0744979c22651
hirosukt <senolynn7584@gmail.com>
2023-04-25 20:43:47 +0900

    Vanilla event canceller

---
+++ src/main/kotlin/love/chihuyu/annihilation/game/VanillaEventCanceller.kt
@@ -0,0 +1,26 @@
+package love.chihuyu.annihilation.game
+
+import org.bukkit.Material
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.block.BlockFadeEvent
+import org.bukkit.event.block.LeavesDecayEvent
+import org.bukkit.event.weather.WeatherChangeEvent
+
+object VanillaEventCanceller: Listener {
+
+    @EventHandler
+    fun onLeave(e: LeavesDecayEvent) {
+        e.isCancelled = true
+    }
+
+    @EventHandler
+    fun onWeather(e: WeatherChangeEvent) {
+        e.isCancelled = true
+    }
+
+    @EventHandler
+    fun onFarm(e: BlockFadeEvent) {
+        e.isCancelled = e.block.type == Material.SOIL
+    }
+}
