2cb0e1b9ce225d0ef93fe4f6f082bde3342162f0
hirosukt <senolynn7584@gmail.com>
2023-05-13 00:41:41 +0900

    Ender furnace

--- src/main/kotlin/love/chihuyu/annihilation/game/handlers/EnderFurnaceHandler.kt
+++ src/main/kotlin/love/chihuyu/annihilation/game/handlers/EnderFurnaceHandler.kt
@@ -1,18 +1,31 @@
 package love.chihuyu.annihilation.game.handlers
 
+import love.chihuyu.annihilation.game.AnnihilationGameManager
+import org.bukkit.Bukkit
+import org.bukkit.Material
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
-import org.bukkit.event.inventory.InventoryOpenEvent
 import org.bukkit.event.inventory.InventoryType
+import org.bukkit.event.player.PlayerInteractEvent
+import org.bukkit.inventory.Inventory
+import java.util.*
 
 object EnderFurnaceHandler: Listener {
 
+    private val enderFurnaces = mutableMapOf<UUID, Inventory>()
+
     @EventHandler
-    fun onOpen(e: InventoryOpenEvent) {
-        val type = e.inventory.type
+    fun onClick(e: PlayerInteractEvent) {
+        val block = e.clickedBlock
+        val player = e.player
+        val furnace = enderFurnaces[player.uniqueId]
+        val currentGame = AnnihilationGameManager.currentGame
 
-        if (type == InventoryType.FURNACE) {
-
+        if (block.type == Material.FURNACE && currentGame != null && block.location in currentGame.map.enderFurnaces) {
+            val inventory = enderFurnaces[player.uniqueId] ?: Bukkit.createInventory(player, InventoryType.FURNACE, "Ender Furnace")
+            if (furnace == null) enderFurnaces[player.uniqueId] = inventory
+            e.isCancelled = true
+            player.openInventory(inventory)
         }
     }
 }
